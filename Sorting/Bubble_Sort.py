363bd4d2bfee18ffe316f539ecb9ba8707ba282e
Gowtham Pollam <gowthampollam@gmail.com>
2019-09-21 18:57:26 +0530

    Sorting File
    The Sorting Directory has the test case file and the Bubble Sort and Selection Sort algorithms

---
+++ Sorting/Bubble_Sort.py
@@ -0,0 +1,22 @@
+import time
+import os
+
+os.chdir(r'C:/Users/Pollam/Desktop/CodeBase/Sorting')
+print(os.getcwd())
+string=list(open('test.txt'))
+l=list(map(int,string[0].split()))
+start=time.time()
+def swap(l,i):
+    temp=l[i]
+    l[i]=l[i+1]
+    l[i+1]=temp
+    return(l)
+n=len(l)
+for j in range(n-1,-1,-1):
+    for i in range(j):
+        if(l[i]>l[i+1]):
+            l=swap(l,i)
+        
+print(l)
+print("Time: ",time.time()-start)
+        
---
+++ Sorting/Selection_sort.py
@@ -0,0 +1,27 @@
+import time
+import os
+
+os.chdir(r'C:/Users/Pollam/Desktop/CodeBase/Sorting')
+print(os.getcwd())
+string=list(open('test.txt'))
+l=list(map(int,string[0].split()))
+start=time.time()
+def swap(l,i,j):
+    temp=l[i]
+    l[i]=l[j]
+    l[j]=temp
+    return(l)
+n=len(l)
+for i in range(n):
+    min=l[i]
+    index=i
+    for j in range(i,n):
+        if(l[j]<min):
+            index=j
+            min=l[j]
+    if(index!=i):
+        swap(l,i,index)
+
+
+print(l)
+print("Time: ",time.time()-start)
