c6369c89ded50c009a2450ed81c3d3b055749279
Gowtham Pollam <gowtham.pollam2018@vitstudent.ac.in>
2020-09-10 13:18:40 +0530

    Update UnBounded KnackSack.py

--- Dp/UnBounded KnackSack.py
+++ Dp/UnBounded KnackSack.py
@@ -1,3 +1,10 @@
+def PrintDP(dp):
+	for i in dp:
+		for j in i:
+			print(j,end="\t")
+		print("")
+		
+#Recursive Approach
 def UnBoundedKS(W,val,wt,n):
 	if(W==0 or n==0):
 		return(0)
@@ -9,7 +16,23 @@ def UnBoundedKS(W,val,wt,n):
 			#Item Ignored			  #Item Taken
 		return(max(UnBoundedKS(W,val,wt,n-1),UnBoundedKS(W-wt[n-1],val,wt,n)+val[n-1]))
 
+#Iterative DP Approach
+ def UnBoundedKS(W,val,wt,n):
+	dp=[[-1 for i in range(W+1)] for j in range(n+1)]
 
+	for i in range(W+1):
+		dp[0][i]=0
+	for i in range(n+1):
+		dp[i][0]=0
+	for i in range(1,n+1):
+		for j in range(1,W+1):
+			if(wt[i-1]>j):
+				dp[i][j]=dp[i-1][j]
+			else:
+				dp[i][j]=max(dp[i][j-wt[i-1]]+val[i-1],dp[i-1][j])
+
+	PrintDP(dp)
+	return(dp[i][j])
 
 
 W=100
