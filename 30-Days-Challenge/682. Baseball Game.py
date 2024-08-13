d330e048d55f6479c035673738bebff0c4340ced
Gowtham Pollam <gowtham.pollam2018@vitstudent.ac.in>
2024-08-12 21:00:04 -0400

    Create 682. Baseball Game.py

---
+++ 30-Days-Challenge/682. Baseball Game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for i in operations:
+            
+            if(i=="+"):
+                stack.append(stack[-1]+stack[-2])
+            elif(i=='D'):
+                stack.append(stack[-1]*2)
+            elif(i=='C'):
+                stack.pop()
+            else:
+                stack.append(int(i))
+          
+
+        return sum(stack)
